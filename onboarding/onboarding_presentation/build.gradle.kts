7dad498e0068e8f03e41967ef1b983cc579eb2b0
OlegKostinDS <olegkostinds@gmail.com>
2024-12-16 19:03:08 +0300

    added dependencies for onboarding presentation module

--- onboarding/onboarding_presentation/build.gradle.kts
+++ onboarding/onboarding_presentation/build.gradle.kts
@@ -39,6 +39,12 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
 }
---
+++ onboarding/onboarding_presentation/src/main/java/com/olegkos/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -0,0 +1,9 @@
+package com.olegkos.onboarding_presentation.welcome
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WelcomeScreen(modifier: Modifier = Modifier) {
+    
+}
